1d3474b692d1a5455e8dc7a64f94dfd610eace68
Neer Patel <neer@patel.com>
2024-12-07 12:09:54 -0500

    init

---
+++ src/main/java/com/neerpatel/spring/basics/spring_5_steps/Spring5StepsApplication.java
@@ -0,0 +1,13 @@
+package com.neerpatel.spring.basics.spring_5_steps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5StepsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5StepsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/neerpatel/spring/basics/spring_5_steps/Spring5StepsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neerpatel.spring.basics.spring_5_steps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5StepsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
